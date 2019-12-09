60a97030861e51889c7b87bbfd8ef5caf8207eb4
avenger999 <1a1a1a.262626.0010@gmail.com>
2019-12-10 03:04:10 +0700

    create user api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :contacts
-    resource :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy, :show]
+    resources :users, only: [:create]
   end
 end
